2a53f8215a305c776b1d457f17f39a8bc4333239
Diego Araújo <diegoedaraujo@gmail.com>
2021-07-30 13:58:56 -0300

    Implementando

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,5 +1,5 @@
 import { useHistory, useParams } from "react-router-dom";
-import logoImg from "../assets/images/log.svg";
+import logoImg from "../assets/images/logo.svg";
 import deleteImg from "../assets/images/delete.svg";
 import checkImg from "../assets/images/check.svg";
 import answerImg from "../assets/images/answer.svg";
